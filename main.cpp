970571e35ec535ea094ad63ee6b74ceeca37875e
Fabrizio David Franco Amayo <39867790+fabriziofranco@users.noreply.github.com>
2019-08-25 14:19:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-
+#include <algorithm>
+#include <map>
+#include <vector>
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
-}
+string transform(string palabra){
+    transform(palabra.begin(),palabra.end(),palabra.begin(),[](char letra){return tolower(letra);});
+    sort(palabra.begin(),palabra.end());
+    return palabra;}
 
+int main() {
+    string palabra; map<string,int> cantidad; vector<string> limpias;
+    while(cin>>palabra && palabra!="#"){
+        string temp=palabra;
+        palabra=transform(palabra);
+        cantidad[palabra]++;
+        if(cantidad[palabra]==1)
+            limpias.push_back(temp);
+        else if(cantidad[palabra]>1){
+            for(int i=0;i<limpias.size();i++){
+                string temp2 = transform(limpias[i]);
+                if(temp2==palabra)
+                    limpias.erase(limpias.begin()+i); } } }
+    sort(limpias.begin(),limpias.end());
+    for(auto & limpia : limpias)
+        cout<<limpia<<endl;
+    return EXIT_SUCCESS;}
